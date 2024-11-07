89fb754b825e2f13b9647ee067f195ec74c8d586
Nikolaos Moskoglou <nikolaosmoskoglou@Nikolaoss-MacBook-Pro.local>
2024-11-07 10:41:57 +0100

    Added validation for firstand last name,email-Added error messages clearing-Added success message

--- nikolaos.js
+++ nikolaos.js
@@ -3,7 +3,7 @@ function containsOnlyLetters(string) {
         if (!(string[i] >= 'A' && string[i] <= 'Z') && !(string[i] >= 'a' && string[i] <= 'z')) {
             return false;
         }
-    return true;
+        return true;
     }
 }
 
@@ -12,13 +12,33 @@ document.getElementById('quizForm').addEventListener('submit', function(event){
 
     event.preventDefault();
 
-    
+    var isValid = true;
 
+    document.getElementById('firstNameError').textContent = "";
+    document.getElementById('lastNameError').textContent = "";
+    document.getElementById('email').textContent = "";
+    document.getElementById('messageDiv').textContent = "";
 
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var emailAddress = document.getElementById('email').value;
 
+    if (!containsOnlyLetters(firstName)) {
+        document.getElementById('firstNameError').textContent = "Please use only letters for first name!";
+        isValid = false;
+    }
 
+    if (!containsOnlyLetters(lastName)) {
+        document.getElementById('lastNameError').textContent = "Please use only letters for last name!";
+        isValid = false;
+    }
 
+    if (!emailAddress.includes("@")){
+        document.getElementById('emailError').textContent = "Please use a valid email address(include @)!";
+        isValid = false;
+    }
 
-    
-
+    if (isValid) {
+        document.getElementById("messageDiv").textContent = "Form submitted succesfully!";
+    }   
 })
